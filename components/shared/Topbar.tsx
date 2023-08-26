f0ace8a91a2d5a76c681c5b4b6ece19c0663f78c
zarifRahman <zarif.shahriar.rahman@gmail.com>
2023-08-26 22:54:48 +0600

    topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,5 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
-// import { dark } from "@clerk/themes";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -27,14 +27,14 @@ function Topbar() {
           </SignedIn>
         </div>
 
-        {/* <OrganizationSwitcher
+        <OrganizationSwitcher
           appearance={{
-            // baseTheme: dark,
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
           }}
-        /> */}
+        />
       </div>
     </nav>
   );
